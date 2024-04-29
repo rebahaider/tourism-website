99aa298295e32caa28947df5a7d2c0b6168c7535
rebahaider <rebahaiderema@gmail.com>
2024-04-30 05:46:42 +0600

    fixed some problems

--- src/components/AllTourists/ViewDetailsPage.jsx
+++ src/components/AllTourists/ViewDetailsPage.jsx
@@ -1,20 +1,34 @@
-// import { useLoaderData } from "react-router-dom";
-// import ViewDetails from "./ViewDetails";
+import { useLoaderData } from "react-router-dom";
+
 
 import Navbar from "../Home/Navbar";
 
 
 const ViewDetailsPage = () => {
 
-    // const loadTourists = useLoaderData();
+    const loadTourists = useLoaderData();
+    const { photo, spotName, countryName, location, shortDescription, averageCost, seasonality, travelTime, totalVisitorsPerYear, email, userName } = loadTourists;
 
     return (
         <div>
             <Navbar></Navbar>
-            <h1>View Details Page</h1>
-            {/* {
-                loadTourists.map(tourist => <ViewDetails key={tourist._id} tourist={tourist}></ViewDetails>)
-            } */}
+            <h2 className="text-3xl font-extrabold text-center">View Details</h2>
+            <div className="card lg:card-side bg-base-100 shadow-xl">
+                <figure><img className="w-[450px] h-[450px] rounded-2xl p-6" src={photo} alt="Album" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title font-bold mb-3">Spot Name : {spotName}</h2>
+                    <p><span className="font-bold">Country Name :</span> {countryName}</p>
+                    <p><span className="font-bold">Location :</span> {location}</p>
+                    <p><span className="font-bold">Short Description : </span>{shortDescription}</p>
+                    <p><span className="font-bold">Average Cost :</span> {averageCost}</p>
+                    <p><span className="font-bold">Seasonality :</span> {seasonality}</p>
+                    <p><span className="font-bold">Travel Time :</span> {travelTime}</p>
+                    <p><span className="font-bold">Total Visitors Per Year :</span> {totalVisitorsPerYear}</p>
+                    <p><span className="font-bold">Email :</span> {email}</p>
+                    <p><span className="font-bold">User Name :</span> {userName}</p>
+
+                </div>
+            </div>
         </div>
 
     );
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProviders";
 import { FaGithub, FaGoogle } from "react-icons/fa";
+import Navbar from "../Home/Navbar";
 
 const Login = () => {
 
@@ -51,49 +52,52 @@ const Login = () => {
     return (
 
 
-        <div className="hero min-h-screen bg-base-200" >
-            <div className="hero-content flex-col">
-                <div className="text-center">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
+        <div>
+            <Navbar></Navbar>
+            <div className="hero min-h-screen bg-base-200" >
+                <div className="hero-content flex-col">
+                    <div className="text-center">
+                        <h1 className="text-5xl font-bold">Login now!</h1>
 
+                    </div>
+                    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 p-6">
+                        <form onSubmit={handleLogin} className="card-body">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-extrabold">Email</span>
+                                </label>
+                                <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-extrabold">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+
+                            </div>
+                        </form>
+                        <p className="text-center font-bold">New To This Site? Please <Link to={"/register"}>
+                            <button className="btn btn-link text-red-600 font-bold">Register</button>
+                        </Link></p>
+
+
+                        {/* Log in with Google */}
+                        <p className="text-center mb-4">
+                            <button onClick={handleSignInWithGoogle} className="btn btn-ghost btn-outline font-extrabold"><FaGoogle></FaGoogle> Log In Using Google Account</button>
+                        </p>
+
+
+                        {/* Login with Github */}
+                        <p className="text-center">
+                            <button onClick={handleSignInWithGithub} className="btn btn-ghost btn-outline font-extrabold"><FaGithub></FaGithub> Log In Using Github Account</button>
+                        </p>
+                    </div>
                 </div>
-                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 p-6">
-                    <form onSubmit={handleLogin} className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text font-extrabold">Email</span>
-                            </label>
-                            <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text font-extrabold">Password</span>
-                            </label>
-                            <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
-
-                        </div>
-                    </form>
-                    <p className="text-center font-bold">New To This Site? Please <Link to={"/register"}>
-                        <button className="btn btn-link text-red-600 font-bold">Register</button>
-                    </Link></p>
-
-
-                    {/* Log in with Google */}
-                    <p className="text-center mb-4">
-                        <button onClick={handleSignInWithGoogle} className="btn btn-ghost btn-outline font-extrabold"><FaGoogle></FaGoogle> Log In Using Google Account</button>
-                    </p>
-
-
-                    {/* Login with Github */}
-                    <p className="text-center">
-                        <button onClick={handleSignInWithGithub} className="btn btn-ghost btn-outline font-extrabold"><FaGithub></FaGithub> Log In Using Github Account</button>
-                    </p>
-                </div>
-            </div>
-        </div >
+            </div >
+        </div>
     );
 };
 
